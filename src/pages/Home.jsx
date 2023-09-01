c9982407c08d5611043b437d878543ed4d75e08c
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-09-01 03:06:38 +0100

    add book layout style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import { BookFormComponent, BookListComponent } from '../components';
 
 const Home = () => (
-  <div>
-    <h1>Welcome to the bookstore</h1>
+  <div className="container">
     <BookListComponent />
     <BookFormComponent />
   </div>
